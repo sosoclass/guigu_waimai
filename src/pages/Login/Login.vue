c20889518d90853f03e7108a46ae09b771ca055f
sosoclass <936598068@qq.com>
2018-12-23 16:08:35 +0800

    login界面组件2

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -5,15 +5,18 @@
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
           <a href="javascript:;" :class="{on:loginWay}" @click="loginWay=true">短信登录</a>
-          <a href="javascript:;" :class="{on:!loginWay}"@click="loginWay=false">密码登录</a>
+          <a href="javascript:;" :class="{on:!loginWay}" @click="loginWay=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
           <div :class="{on:loginWay}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!isRightPhone||computeTime>0" class="get_verification"
+                      :class="{right_phone_number:isRightPhone}" @click.prevent="sendCode">
+                {{computeTime > 0 ? `已发送(${computeTime}s)` : `获取验证码`}}
+              </button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -29,10 +32,10 @@
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input :type="isShowPwd?'password':'text'" maxlength="8" placeholder="密码" >
+                <div class="switch_button off" @click="isShowPwd=!isShowPwd" :class="isShowPwd?'on':'off'">
+                  <div class="switch_circle" :class="{right:isShowPwd}"></div>
+                  <span class="switch_text">{{isShowPwd?'abc':''}}</span>
                 </div>
               </section>
               <section class="login_message">
@@ -55,12 +58,28 @@
 <script>
   export default {
     data(){
-      return{
-        loginWay:true,//true短信登录 false密码登录
-        phone:'',
-        computeTime:0,
-        isShowPwd:false
+      return {
+        loginWay: true,//true短信登录 false密码登录
+        phone: '',
+        computeTime: 0,
+        isShowPwd: false
 
+      }
+    },
+    computed: {
+      isRightPhone(){
+        return /^1\d{10}$/.test(this.phone)
+      }
+    },
+    methods: {
+      sendCode(){
+        this.computeTime = 30
+        const IntervalId =  setInterval(()=>{
+          this.computeTime--
+          if(this.computeTime<=0){
+            clearInterval(IntervalId)
+          }
+        },1000)
       }
     }
   }
@@ -86,7 +105,7 @@
         .login_header_title
           padding-top 40px
           text-align center
-          >a
+          > a
             color #333
             font-size 14px
             padding-bottom 4px
@@ -97,8 +116,8 @@
               font-weight 700
               border-bottom 2px solid #02a774
       .login_content
-        >form
-          >div
+        > form
+          > div
             display none
             &.on
               display block
@@ -128,6 +147,8 @@
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right_phone_number
+                  color black
             .login_verification
               position relative
               margin-top 16px
@@ -138,7 +159,7 @@
                 font-size 12px
                 border 1px solid #ddd
                 border-radius 8px
-                transition background-color .3s,border-color .3s
+                transition background-color .3s, border-color .3s
                 padding 0 6px
                 width 30px
                 height 16px
@@ -155,7 +176,7 @@
                     color #ddd
                 &.on
                   background #02a774
-                >.switch_circle
+                > .switch_circle
                 //transform translateX(27px)
                   position absolute
                   top -1px
@@ -165,14 +186,16 @@
                   border 1px solid #ddd
                   border-radius 50%
                   background #fff
-                  box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
+                  box-shadow 0 2px 4px 0 rgba(0, 0, 0, .1)
                   transition transform .3s
+                  &.right
+                    transform translateX(27px)
             .login_hint
               margin-top 12px
               color #999
               font-size 14px
               line-height 20px
-              >a
+              > a
                 color #02a774
           .login_submit
             display block
@@ -198,7 +221,7 @@
         left 5px
         width 30px
         height 30px
-        >.iconfont
+        > .iconfont
           font-size 20px
           color #999
 </style>
